490850734b84725b94a7a9a1fc79862139dadfca
Evan Bubniak <evan.bubniak@gmail.com>
2018-12-06 11:20:58 -0500

    Restructured document, added AJAX call for current comic number

--- newtab.js
+++ newtab.js
@@ -1,17 +1,25 @@
-let i;
-for (i = 0; i<=4; i++)
-{
-let j = i;
-var comicnumber = Math.floor((Math.random()*2080) + 1);
+var currentComicNumber;
 
 $.ajax({
-    url: "http://xkcd.com/" + comicnumber + "/info.0.json",
+    url: "http://xkcd.com/info.0.json",
     dataType: "json",
     success: function(input) {
-        $("#xkcdimg" + String(j) + "").html("<img src = \"" + input.img + "\" />");
-        console.log(input.img);
-        console.log("#xkcdimg" + String(j) + "")
+        currentComicNumber = input.num;
+        console.log(currentComicNumber);
+        let i;
+        for (i = 0; i<=4; i++) {
+            let j = i;
+            var comicNumber = Math.floor((Math.random()*currentComicNumber) + 1);
+            $.ajax({
+                url: "http://xkcd.com/" + comicNumber + "/info.0.json",
+                dataType: "json",
+                success: function(input) {
+                    $("#xkcdimg" + String(j) + "").html("<img src = \"" + input.img + "\" />");
+                }
+            })
+        //console.log(input.img);
+        //console.log("#xkcdimg" + String(j) + "")
+        }
     }
-})
+});
 
-}
