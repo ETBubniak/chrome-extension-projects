a16c87feb38504c6a910c63e1e76a740726bdc4d
Evan Bubniak <evan.bubniak@gmail.com>
2018-12-07 18:40:22 -0500

    Switched Ajax request to synchronous to make it work - will have to modify code to work in asynchronous later. Restructured JS significantly to allow chronological ordering of comics.

--- newtab.js
+++ newtab.js
@@ -1,4 +1,54 @@
-function getImages() {
+function getImages() {    
+    var numberOfComics = parseInt($("#numberOfComics").val());
+    document.getElementById("imageContainer").innerHTML = "";
+    var currentComicNumber;
+    (function(){
+        $.ajax({
+            url: "http://xkcd.com/info.0.json",
+            dataType: "json",
+            success: function (input) {
+                
+                //Build a list of objects to store all needed properties.
+                currentComicNumber = input.num;
+                var comicObjects = [];
+                let i;
+                for (i=1; i<=numberOfComics;i++){
+                    let j = i;
+                    var comicNumber = Math.floor((Math.random()*currentComicNumber) + 1);
+                    $.ajax({
+                        url: "http://xkcd.com/" + comicNumber + "/info.0.json",
+                        dataType: "json",
+                        async: false,
+                        success: function(input) {
+                            let objectToAppend = input;
+                            comicObjects.push(objectToAppend); 
+                        }
+                    })
+                }
+                comicObjects.sort(function(a,b) {
+                    return a.num - b.num
+                });
+                console.log(comicObjects);
+                console.log(comicObjects.length);
+                for (i=0; i<comicObjects.length; i++){
+                    console.log("completed");
+                    let j = i;
+                    document.getElementById("imageContainer").innerHTML += "<img src=\"" + comicObjects[j].img + "\" <br><p>\"" + comicObjects[j].alt + "\"</p><hr>"
+                }
+            }
+        })
+    })();
+
+}
+
+
+window.onload = function () {
+    document.getElementById("submitButton").addEventListener("click", function(event) {
+        event.preventDefault();
+        getImages()
+    }, false);
+}
+/* function getImages() {
     
     var numberOfComics = parseInt($("#numberOfComics").val());
     var currentComicNumber;
@@ -34,4 +84,4 @@ window.onload = function () {
         event.preventDefault();
         getImages()
     }, false);
-}
+} */
