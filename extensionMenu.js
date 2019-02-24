e032d3770a56d6e633c5e8132158ec35eb5e0473
Evan Bubniak <evan.bubniak@gmail.com>
2019-02-24 10:28:43 -0500

    Significant refactoring to adhere to functional programming

--- extensionMenu.js
+++ extensionMenu.js
@@ -1,25 +1,48 @@
-function createLinksFromBookmarks(folderId){
-    const linksToReturn = [];
-    chrome.bookmarks.getChildren(folderId,
+function getTargetFolderID() {
+    const newsFolderID = 110;
+    return String(newsFolderID);
+}
+
+function getContentsOfFolder(folderID){
+    const ID = folderID;
+    chrome.bookmarks.getChildren(ID,
         (results) => {
-            const arrayOfResults = results;
-            console.log(arrayOfResults);
-            const arrayOfUrlResults = arrayOfResults.filter(
-                (result) => {
-                    return ('url' in result)
-                }
-            );
-            const arrayOfFolderResults = arrayOfResults.filter(
-                function(result) {
-                    return !('url' in result)
-                }
-            );
-            console.log(arrayOfUrlResults);
-            console.log(arrayOfFolderResults);
+            return results;
         });
 }
 
-function openBookmarks(arrayOfUrls){
+function separateIntoUrlsAndFolders(results){
+    const urlResults = results.filter(
+        (result) => {
+            return ('url' in result)
+        }
+    );
+    const folderResults = results.filter(
+        function(result) {
+            return !('url' in result)
+        }
+    );
+    return [urlResults, folderResults];
+}
+
+function getUrlFromUrlResults(urlResults){
+    const randomIndex = Math.floor((Math.random()*urlResults.length) + 1)
+    const urlToAdd = urlResults[randomIndex].url;
+    return urlToAdd;
+}
+
+function getUrlsfromFolder(folderID){
+    const urlsToReturn = [];
+    const results = getContentsOfFolder(folderID);
+    const [urlResults, folderResults] = separateIntoUrlsAndFolders(results);
+    urlsToReturn.push(getUrlFromUrlResults(urlResults));
+    for (let folder in folderResults){
+        urlsToReturn = urlsToReturn.concat(getUrlsfromFolder(folder.id))
+    }
+    return urlsToReturn;
+}
+
+function openBookmarks(urlsToOpen){
     for (let url of arrayOfUrls){
         chrome.tabs.create({
             "url": url 
@@ -27,50 +50,18 @@ function openBookmarks(arrayOfUrls){
     }
 }
 
-function printBookmarks(id) {
-    chrome.bookmarks.get(id,
-        (results) => {
-            console.log(results);
-            console.log("id = " + results[0].id + ", title = " + results[0].title);
-        });
-    chrome.bookmarks.getChildren(id,
-        (results) => {
-            console.log(results);
-            for (let result in results) {
-                console.log("id = " + result.id + ", title = " + result.title);
-            }
-        });
+function main() {
+    const newsFolderID = getTargetFolderID();
+    const urlsToOpen = [];
+    urlsToOpen = urlsToOpen.concat(getUrlsfromFolder(newsFolderID));
+    openBookmarks(urlsToOpen);
 }
 
-function printBookmarksRecursively(id) {
-    console.log("fired function printBookmarks, id = " + id);
-    chrome.bookmarks.get(id, (results) => {
-        console.log("id = " + results[0].id + ", title = " + results[0].title);
-        console.log(results);
-    });
-    chrome.bookmarks.getChildren(id, (results) => {
-        console.log(results);
-        for (let result of results) {
-            printBookmarks(result.id);
-        }
-    });
-
-}
-
-window.onload = function () {
-    bookmarksBarId = "1";
-    newsFolderId = "110";
+window.onload = () => {
     document
     .getElementById("openBookmarks")
     .addEventListener("click", function(event){
         event.preventDefault();
-        createLinksFromBookmarks(newsFolderId);
-        // openBookmarks(arrayOfUrlsToOpen);
-    });
-    document
-    .getElementById("betaConsoleLogBookmarks")
-    .addEventListener("click", function(event){
-        event.preventDefault();
-        printBookmarks(newsFolderId);
+        main();
     });
 }
